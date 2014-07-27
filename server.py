71d0f7727c39057f952acd672b35a095d7d2f63b
Raffael Stahl <raffael.stahl@gmail.com>
2014-07-27 20:32:55 +0200

    work in progress

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ import struct
 UDP_SERVER_RESPONSE = "\x80"
 UDP_CLIENT_REQUEST = "\x00"
 
-class HostedGame():
-    pass 
 
 """
 len     type            val
@@ -14,27 +12,16 @@ len     type            val
 1 byte  "mode"          hex(00)=>request(client->server), hex(80)=>response(server->client)   
 1 byte  "content-len"   hex encoded content length
 
-
-game_mode=3;inet=1;maxpl=1;nation=de;numpl=1;password=0;players=§(10)←Patric
-k∟§(-1)←;run=1;server_addr=62.143.7.179;server_name=tot98 (Patrick);server_port=
-58282;vmaj=1;vmin=3;vtype=f
-
-game=EM4;lang=de;mod=
 """
-
+class HostedGame():
+    pass 
 
 class CommunityServer():
     def __init__(self):
         self.games = []
-    def addGame(self, client, request):
-        pass
-    def endGame(self, client):
-        pass
-    def getGames(self):
-        return self.games
+    #Byte 3 should be mode \x00, ignore. Byte 4 should be content-len, ignore
     def processUDP(self, data):
-        session = data[:2]
-        #Byte 3 should be mode \x00, ignore. Byte 4 should be content-len, ignore
+        session = data[:2]  
         request = data[4:]
         print request
         response = "game_mode=3;inet=1;maxpl=4;nation=de;numpl=1;password=0;players=\xA7(10)\x1BnoBlubb\x1C\xA7(-1)\x1B;run=1;server_addr=88.76.57.26;server_name=Blubberium (noBlubb);server_port=58282;vmaj=1;vmin=1;vtype=f"
@@ -44,7 +31,7 @@ class CommunityServer():
 class TCPFactory(protocol.Protocol):   
     def __init__(self, master):
         self.server = master
-    def doStart():
+    def doStart(self):
         pass
     def dataReceived(self, data):
         self.transport.write(data)
@@ -53,10 +40,10 @@ class TCPFactory(protocol.Protocol):
 class UDPFactory(protocol.DatagramProtocol):
     def __init__(self, master):
         self.server = master
-    def doStart():
+    def doStart(self):
         pass
     def dataReceived(self, data, address):
-        response = self.server.processUDP(request)
+        response = self.server.processUDP(data)
         self.transport.write(response, address)
 
 
