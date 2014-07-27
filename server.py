fe7eac92672f6f53281358aede71fa8864ed096d
Raffael Stahl <raffael.stahl@gmail.com>
2014-07-27 21:40:15 +0200

    UDP tested and working

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import struct
 
 UDP_SERVER_RESPONSE = "\x80"
 UDP_CLIENT_REQUEST = "\x00"
-
+UDP_DEBUGR_RESPONSE = "game_mode=3;inet=1;maxpl=4;nation=de;numpl=1;password=0;players=\xA7(10)\x1BnoBlubb\x1C\xA7(-1)\x1B;run=1;server_addr=127.0.0.1;server_name=Blubberium (noBlubb);server_port=58282;vmaj=1;vmin=1;vtype=f"
 
 """
 len     type            val
@@ -11,10 +11,28 @@ len     type            val
 2 bytes "session-id"    random?
 1 byte  "mode"          hex(00)=>request(client->server), hex(80)=>response(server->client)   
 1 byte  "content-len"   hex encoded content length
-
 """
-class HostedGame():
-    pass 
+def HELPER_buildNameString(playerList):
+    if len(playerList) == 1:
+        return "\xA7(10)\x1B{0:s}\x1C\xA7(-1)\x1B".format(playerList[0])
+    elif len(playerList) == 2:
+        return "\xA7(10)\x1B{0:s}\x1C\xA7(-1)\x1B, {1:s}".format(playerList[0],playerList[1])
+    elif len(playerList) > 2:
+        return "\xA7(10)\x1B{0:s}\x1C\xA7(-1)\x1B, ".format(playerList[0]) + ', '.join(playerList[1:4])
+
+def HELPER_dictionaryToDataString(dataIn):
+    conversion = ''
+    for x,y in dataIn:
+        conversion += ';' + x + '=' + y
+    return conversion
+
+def HELPER_dataStringToDictionary(dataIn):
+    conversion = {}
+    for x in dataIn.split(';'):
+        x,y = x.split('=')
+        if len(x) > 0:
+            conversion[x] = y
+    return conversion
 
 class CommunityServer():
     def __init__(self):
@@ -24,7 +42,7 @@ class CommunityServer():
         session = data[:2]  
         request = data[4:]
         print request
-        response = "game_mode=3;inet=1;maxpl=4;nation=de;numpl=1;password=0;players=\xA7(10)\x1BnoBlubb\x1C\xA7(-1)\x1B;run=1;server_addr=88.76.57.26;server_name=Blubberium (noBlubb);server_port=58282;vmaj=1;vmin=1;vtype=f"
+        response = UDP_DEBUGR_RESPONSE
         responselen = struct.pack('B', len(response))
         return session + UDP_SERVER_RESPONSE + responselen + response
 
@@ -42,7 +60,7 @@ class UDPFactory(protocol.DatagramProtocol):
         self.server = master
     def doStart(self):
         pass
-    def dataReceived(self, data, address):
+    def datagramReceived(self, data, address):
         response = self.server.processUDP(data)
         self.transport.write(response, address)
 
